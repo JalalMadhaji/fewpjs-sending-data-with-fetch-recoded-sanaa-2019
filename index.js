568b2295c753e16b2073267c076309a88e2211c9
Jalal Al-Madhaji <jalal.almadhaji90@gmail.com>
2019-10-08 20:32:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your code here
+const body = document.body;
+function submitData(name, email){
+  let obj = {
+    name:name,
+    emial:email
+  }
+  return fetch('http://localhost:3000/users',{
+    method:"POST",
+    headers:{
+      "content-type":"application/json",
+      "accept":"application/json"
+    },
+    body:json.stringify(obj)
+  }).then((jsonRes)=>jsonRes.json()).then((res)=>body.innerHTML = res.message.id).catch((err)=>body.innerHTML = err.message);
+}
